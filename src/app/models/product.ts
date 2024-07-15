e192367d1ff046bed2c3dbcea4eb7d8ba11857c8
Bohdan Zhykhor <bohdanzhyhor@gmail.com>
2024-07-14 23:22:45 -0400

    created empty pages and structure for products

---
+++ src/app/models/product.ts
@@ -0,0 +1,14 @@
+export interface Product {
+  id: string;
+  imgUrl: string;
+  price: number;
+  discount: number;
+  shops: string[];
+  tags: string[];
+  name: string;
+  ingredients: string[];
+  discountUntil: string;
+  isNew: boolean;
+  color: string[];
+  size: string[];
+}
---
+++ src/app/models/tag.ts
@@ -0,0 +1,10 @@
+export enum Tag {
+  Drink = 'drink',
+  Meal = 'meal',
+  Pizza = 'pizza',
+  Sauce = 'sauce',
+  Soup = 'soup',
+  Roll = 'roll',
+  Salad = 'salad',
+  Alcohol = 'alcohol',
+}
