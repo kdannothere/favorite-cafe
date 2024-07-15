9200259f398cdb46822fca5d6a8b135d9ce1ccba
Bohdan Zhykhor <bohdanzhyhor@gmail.com>
2024-07-14 23:31:33 -0400

    added some more tags

--- src/app/models/tag.ts
+++ src/app/models/tag.ts
@@ -1,10 +1,12 @@
 export enum Tag {
   Drink = 'drink',
   Meal = 'meal',
+  ForKids = 'for kids',
   Pizza = 'pizza',
   Sauce = 'sauce',
   Soup = 'soup',
   Roll = 'roll',
   Salad = 'salad',
   Alcohol = 'alcohol',
+  AlcoholZero = 'alcohol zero',
 }
