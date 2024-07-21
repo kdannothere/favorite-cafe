4bf59eaa422ee0e3685670601b802d140f868ce2
Bohdan Zhykhor <bohdanzhyhor@gmail.com>
2024-07-21 00:05:48 -0400

    fixed animation not working bug when deployed

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
